0499a452fe6cb45ddb07f875f99c900bc7ddb42c
Uzike <133394137+Uzike@users.noreply.github.com>
2023-06-14 19:43:51 +0400

    Create envirobot.py

---
+++ envirobot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands 
+import requests
+from my_token import my_token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='%', intents=intents)
+##########################################################################
+url1 = "https://climate-change-live402.p.rapidapi.com/news/nyp"
+
+headers1 = {
+	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+	"X-RapidAPI-Host": "climate-change-live402.p.rapidapi.com"
+}
+##########################################################################
+
+@bot.event
+async def on_ready():
+    print("All good.")
+
+@bot.command('climatechange')
+async def climatechange(ctx):
+    responce1 = requests.get(url1, headers=headers1)
+    await ctx.send(responce1.json())
+
+
+bot.run("my_token")
